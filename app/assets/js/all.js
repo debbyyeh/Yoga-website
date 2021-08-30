6864fba3b00d18930bc4d170115b92602819f507
debbyyeh <app2984@gmail.com>
2021-08-30 11:05:23 +0800

    edit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+    $('.header-menu-btn').click(function (e) {
+        e.preventDefault();
+        $('body').toggleClass('header-height-show');
+    });
+    $('.reservation-btn').click(function (e) {
+        e.preventDefault();
+        $('.reservation').toggleClass('reservation-height-show');
+    });
+    $('.courses_level').click(function (e) {
+        e.preventDefault();
+        $(this).parent().toggleClass('.active');
+    });
+    $('.order-btn').click(function (e) {
+        e.preventDefault();
+        $('.reserveFinish').toggleClass('order-height-show');
+    });
 });
