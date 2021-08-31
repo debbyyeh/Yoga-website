3f3d7cd4d9692f1d6e2a6aed1129985aaa40cae3
debbyyeh <app2984@gmail.com>
2021-08-31 23:40:36 +0800

    Update 2021-08-31T15:40:32.439Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -9,6 +9,18 @@ $(document).ready(function () {
     e.preventDefault();
     $('.reservation').toggleClass('reservation-height-show');
   });
+  $('.level-btn-1').click(function (e) {
+    e.preventDefault();
+    $('.reservation').toggleClass('level-height-show-1');
+  });
+  $('.level-btn-2').click(function (e) {
+    e.preventDefault();
+    $('.reservation').toggleClass('level-height-show-2');
+  });
+  $('.level-btn-3').click(function (e) {
+    e.preventDefault();
+    $('.reservation').toggleClass('level-height-show-3');
+  });
   $('.courses_level').click(function (e) {
     e.preventDefault();
     $(this).parent().toggleClass('.active');
